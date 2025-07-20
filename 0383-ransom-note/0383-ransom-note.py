c026b14cd3382a32f0aa7357a8c4565b34c1fd45
Qaisar Abbas <161966447+QaisarAbbas2024@users.noreply.github.com>
2025-07-20 16:50:14 +0500

    Time: 27 ms (71.81%), Space: 12.8 MB (15.35%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        
+        #Step 1: Create An empty dictionary
+        letter_count = {}
+
+        #Step 2: Count letters in magazine
+        for letter in magazine:
+            if letter in letter_count:
+                letter_count[letter] += 1
+            else:
+                letter_count[letter] = 1
+       
+
+        #Step 3: Check if we can build ransomnote
+        for letter in ransomNote:
+            if letter in letter_count and letter_count[letter] > 0:
+                letter_count[letter] -= 1
+            else:
+                return False
+        return True
+
+        
